bfd709f45c21db3f6dd822fac2bb5c6b6a97dd01
Sam Woolerton <samuelwoolerton@gmail.com>
2021-05-17 20:32:49 +1200

    Build test query as proof of concept to get passage details

---
+++ data/test_query.js
@@ -0,0 +1,60 @@
+const { createClient } = require("@supabase/supabase-js")
+
+async function main() {
+  const supabase = createClient(
+    "https://fismpnesaaiuvklctjqh.supabase.co",
+    // public anon key so safe to commit to Git
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMTIzNTMwNCwiZXhwIjoxOTM2ODExMzA0fQ.stWtUi-RZS9dDN3aRctpDgpsMQpn5kcEez2zfqkb_t8"
+  )
+
+  const res = await supabase.from("Passages").select(`
+    start_verse,
+    end_verse,
+    from:start_verse (
+      chapter (
+        number,
+        book ( name )
+      )
+    )
+  `)
+  const [{ start_verse, end_verse, from }] = res.data
+
+  const book = from.chapter.book.name
+  const chapter = from.chapter.number
+
+  const meta = {
+    book,
+    chapter,
+    start_verse,
+    end_verse,
+  }
+
+  const { data: verses } = await supabase
+    .from("Verses")
+    .select(
+      `
+        number,
+        text
+      `
+    )
+    .gte("id", start_verse)
+    .lte("id", end_verse)
+
+  const verseText = verses.map(({ text }) => text)
+  const wordCount = verseText.reduce((acc, t) => acc + t.split(" ").length, 0)
+
+  const fullText = verses.map(({ text }) => text).join(" ")
+  const getChunks = str =>
+    str
+      .split(/[\.,!;:]/g)
+      .map(c => c.trim())
+      .filter(c => c !== "")
+  const chunks = getChunks(fullText)
+
+  console.log("Metadata", meta)
+  // console.log("verses", verses)
+  console.log("Word count", wordCount)
+  console.log("Chunks", chunks)
+}
+
+main()
