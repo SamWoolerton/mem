943f99ef0e77a6977da5c845c7b7c1d38043a4c2
Sam Woolerton <samuelwoolerton@gmail.com>
2021-05-21 17:29:25 +1200

    Fix next.config.js to get Tailwind styles back

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const bsconfig = require("./bsconfig.json")
 const fs = require("fs")
+const withPlugins = require("next-compose-plugins")
 const withPWA = require("next-pwa")
 
 const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"])
@@ -25,46 +26,42 @@ function patchResDeps() {
 }
 patchResDeps() // update package.json and create empty `index.js` before transpiling
 
-const isWebpack5 = true
 const config = {
   target: "serverless",
   pageExtensions: ["jsx", "js"],
-  env: {
-    ENV: process.env.NODE_ENV,
-  },
+  env: { ENV: process.env.NODE_ENV },
   webpack: (config, options) => {
     const { isServer } = options
 
-    if (isWebpack5) {
-      if (!isServer) {
-        // We shim fs for things like the blog slugs component
-        // where we need fs access in the server-side part
-        config.resolve.fallback = {
-          fs: false,
-          path: false,
-        }
+    if (!isServer) {
+      // We shim fs for things like the blog slugs component
+      // where we need fs access in the server-side part
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
       }
-
-      // We need this additional rule to make sure that mjs files are
-      // correctly detected within our src/ folder
-      config.module.rules.push({
-        test: /\.m?js$/,
-        use: options.defaultLoaders.babel,
-        exclude: /node_modules/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      })
     }
+
+    // We need this additional rule to make sure that mjs files are
+    // correctly detected within our src/ folder
+    config.module.rules.push({
+      test: /\.m?js$/,
+      use: options.defaultLoaders.babel,
+      exclude: /node_modules/,
+      type: "javascript/auto",
+      resolve: { fullySpecified: false },
+    })
+
     return config
   },
   future: {
-    webpack5: isWebpack5,
+    webpack5: true,
   },
 }
 
-module.exports = withPWA({
-  pwa: { dest: "public", disable: true },
-  ...withTM(config),
-})
+module.exports = withPlugins([
+  withPWA({
+    pwa: { dest: "public", disable: true },
+  }),
+  withTM(config),
+])
