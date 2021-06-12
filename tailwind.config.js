0f91eb789e3085ea04cafcbad3ecb3cc384aef05
Sam Woolerton <samuelwoolerton@gmail.com>
2021-06-12 20:49:37 +1200

    Fix PurgeCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   purge: {
     // Specify the paths to all of the template files in your project
-    content: [
-      "./src/components/**/*.res",
-      "./src/layouts/**/*.res",
-      "./src/*.res",
-    ],
+    content: ["./src/**/*.res"],
     options: {
       safelist: ["html", "body"],
     },
